20f8ac9c1dc59303f604f0ffb9ea0e52b847df42
AbdulQadir <abdulessa93@gmail.com>
2022-06-29 08:55:30 -0400

    remove extra console in dept

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'admin',
-    password: 'admin',
+    password: '',
     database: 'employee_history'
 })
 
